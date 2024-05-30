d28a6e457484a8a436c5fc4e21e75fba96d8288c
Peculiar1129 <168407066+Peculiar1129@users.noreply.github.com>
2024-05-31 01:16:25 +0800

    Update script.js

--- script.js
+++ script.js
@@ -29,38 +29,5 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 });
 
-const images = document.querySelectorAll('img');
 
-function getRandomColor() {
-    const letters = '0123456789ABCDEF';
-    let color = '#';
-    for (let i = 0; i < 6; i++) {
-        color += letters[Math.floor(Math.random() * 16)];
-    }
-    return color;
-}
-
-images.forEach(img => {
-    img.addEventListener('mouseover', () => {
-        img.style.transform = 'scale(1.1)';
-        img.style.border = `8px solid ${getRandomColor()}`;
-        img.style.boxShadow = '0 0 10px rgba(255, 255, 255, 0.8)';
-        img.style.filter = 'brightness(1.1)';
-    });
-
-    img.addEventListener('mouseout', () => {
-        img.style.transform = 'scale(1)';
-        img.style.border = 'none';
-        img.style.boxShadow = 'none';
-        img.style.filter = 'brightness(1)';
-    });
-});
-
-
-document.addEventListener('DOMContentLoaded', () => {
-    const img = document.querySelector('.blur-load');
-    img.addEventListener('load', () => {
-        img.classList.add('loaded');
-    });
-});
 
